70bbf902f17c5e17a5450e3da840099ce2475320
Sourav Basak <110307917+souravbsk@users.noreply.github.com>
2024-09-10 16:17:04 +0600

    Update auth.controllers.js

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -99,10 +99,11 @@ const googleLoginCallback = async (req, res) => {
         expiresIn: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 20,
       }
     );
-  res.cookie("jwt-token", token, {
-  httpOnly: true,    // Prevents JavaScript from accessing the cookie
-  sameSite: "Strict", // Helps prevent CSRF attacks
-  maxAge: 60 * 60 * 24 * 20 * 1000, // Set expiration for 20 days
+res.cookie("jwt-token", token, {
+  path: "/",
+  httpOnly: true,
+  sameSite: "Strict",
+  maxAge: 60 * 60 * 24 * 20 * 1000,
 });
 
     res.redirect("https://www.sabhyasha.com/store");
