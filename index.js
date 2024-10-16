7c8513ea280a594f1ece2282d8a519fab3fa0e37
sabhyasha <113874682+sabhyasha@users.noreply.github.com>
2024-10-16 23:55:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '0.0.0.0';
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
