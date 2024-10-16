f48a23c9aaf17e53b826df263c42422027c0cce5
VinitRathi-dotcom <vinit@sabhyasha.com>
2024-10-16 23:18:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hello World! Update\n');
 });
 
 server.listen(port, hostname, () => {
