1a16ba1facf18443c413794a6e59a903fa3a867f
VinitRathi-dotcom <vinit@sabhyasha.com>
2024-10-17 07:38:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.end('Hello World! The CICD pipeline has been created Successfully\n');
 });
 
 server.listen(port, hostname, () => {
