38f4b6b1f64830165829cc824692134b1207ce70
Sourav Basak <souravbsk01@gmail.com>
2024-10-21 12:07:25 +0600

    modify html files

--- routes/ondcVerify.routes.js
+++ routes/ondcVerify.routes.js
@@ -10,7 +10,7 @@ const ondcVerifyRoute = require("express").Router();
 // create blog category
 
 ondcVerifyRoute.post("/on_subscribe", onSubscribe);
-ondcVerifyRoute.get("/ondc-site-verification.html", serveVerificationFile);
+ondcVerifyRoute.get("/ondc-site-verification", serveVerificationFile);
 ondcVerifyRoute.get("/health", healthCheck);
 ondcVerifyRoute.post("/callback", handleCallback);
 module.exports = { ondcVerifyRoute };
